dd7845e9835a5e066f345bda21597791a8210dda
enverbisevac <enver@bisevac.com>
2023-11-14 09:04:05 +0100

    top level function for std error package

--- errors/std.go
+++ errors/std.go
@@ -2,14 +2,31 @@ package errors
 
 import "errors"
 
+// New returns an error that formats as the given text.
+// Each call to New returns a distinct error value even if the text is identical.
 func New(text string) error {
 	return errors.New(text)
 }
 
+// Is reports whether any error in err's tree matches target.
 func Is(err error, target error) bool {
 	return errors.Is(err, target)
 }
 
+// As finds the first error in err's tree that matches target, and if one is found, sets
+// target to that error value and returns true. Otherwise, it returns false.
 func As(err error, target any) bool {
 	return errors.As(err, target)
+}
+
+// Unwrap returns the result of calling the Unwrap method on err, if err's
+// type contains an Unwrap method returning error.
+// Otherwise, Unwrap returns nil.
+func Unwrap(err error) error {
+	return errors.Unwrap(err)
+}
+
+// Join returns an error that wraps the given errors.
+func Join(errs ...error) error {
+	return errors.Join(errs...)
 }
