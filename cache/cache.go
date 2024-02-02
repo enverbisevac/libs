6f7916ff8ef09ae1a6165ce7fb13cbd9bb00ab9a
enverbisevac <enver@bisevac.com>
2024-02-02 03:05:14 +0100

    Get helper TLF

--- cache/cache.go
+++ cache/cache.go
@@ -9,3 +9,60 @@ type Cache interface {
 	Pop(key string) (any, error)
 	Keys(prefix string) []string
 }
+
+type config struct {
+	AsyncSetter   bool
+	OnSetterError func(err error)
+}
+
+type CacheConfigFunc func(*config)
+
+func OnError(fn func(err error)) CacheConfigFunc {
+	return func(c *config) {
+		c.OnSetterError = fn
+	}
+}
+
+func AsyncSetter(value bool) CacheConfigFunc {
+	return func(c *config) {
+		c.AsyncSetter = value
+	}
+}
+
+func Get[T any](cache Cache, key string, fetcher func() (T, error), options ...CacheConfigFunc) (T, error) {
+	var (
+		zero T
+		c    config
+	)
+
+	for _, f := range options {
+		f(&c)
+	}
+
+	if value, err := cache.Get(key); err == nil && value != nil {
+		output, ok := value.(T)
+		if ok {
+			return output, nil
+		}
+	}
+
+	output, err := fetcher()
+	if err != nil {
+		return zero, err
+	}
+
+	setter := func() {
+		err = cache.Set(key, output, 1*time.Hour)
+		if err != nil {
+			c.OnSetterError(err)
+		}
+	}
+
+	if c.AsyncSetter {
+		go setter()
+	} else {
+		setter()
+	}
+
+	return output, nil
+}
